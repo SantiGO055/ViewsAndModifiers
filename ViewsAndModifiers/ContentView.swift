b0df8fc5025caef14cb0d2b45aa66bb461412913
SantiGO055 <santigonzalez05@gmail.com>
2023-12-20 21:06:37 -0300

    .

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var useRedText = false
-    
+    // views like properties
     let motto1 = Text("Draco dormiens")
     let motto2 = Text("nunquam titillandus")
+    
+    //computed properties
+    var motto3: some View {
+        Text("Draco dormiens")
+    }
+    
+    // cuando se separa en variables mas de una vista hay que agrupar por vstack o Group:
+    var spells: some View {
+        Group {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    // o agregar a mano el viewbuilder :
+    @ViewBuilder var spells1: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
     
     var body: some View {
         Button("Hello World") {
@@ -39,9 +57,15 @@ struct ContentView: View {
         .blur(radius: 5)
         
         VStack {
-                    motto1
-                    motto2
-                }
+            motto1
+            motto2
+        }
+        VStack {
+            motto1
+                .foregroundStyle(.red)
+            motto2
+                .foregroundStyle(.blue)
+        }
         
 
     }
