23c62dcfc1e79be63ad583699d132852a142d6fd
SantiGO055 <santigonzalez05@gmail.com>
2023-12-20 21:21:07 -0300

    .

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,67 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var useRedText = false
-    // views like properties
-    let motto1 = Text("Draco dormiens")
-    let motto2 = Text("nunquam titillandus")
-    
-    //computed properties
-    var motto3: some View {
-        Text("Draco dormiens")
-    }
-    
-    // cuando se separa en variables mas de una vista hay que agrupar por vstack o Group:
-    var spells: some View {
-        Group {
-            Text("Lumos")
-            Text("Obliviate")
-        }
-    }
-    // o agregar a mano el viewbuilder :
-    @ViewBuilder var spells1: some View {
-        Text("Lumos")
-        Text("Obliviate")
-    }
-    
-    var body: some View {
-        Button("Hello World") {
-            // flip the Boolean between true and false
-            useRedText.toggle()
-        }
-        .foregroundStyle(useRedText ? .red : .blue)
-        
-        VStack {
-            Text("Gryffindor")
-                .font(.largeTitle) // child takes priority
-            Text("Hufflepuff")
-            Text("Ravenclaw")
-            Text("Slytherin")
-        }
-        .font(.title)
-        
-        VStack {
-            Text("Gryffindor")
-                .blur(radius: 0)
-            Text("Hufflepuff")
-            Text("Ravenclaw")
-            Text("Slytherin")
-        }
-        .blur(radius: 5)
-        
-        VStack {
-            motto1
-            motto2
-        }
-        VStack {
-            motto1
-                .foregroundStyle(.red)
-            motto2
-                .foregroundStyle(.blue)
-        }
-        
+// separamos en un componente aparte
+struct CapsuleText: View {
+    var text: String
 
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+        }
     }
 }
 
